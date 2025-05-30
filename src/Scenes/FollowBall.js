159fedbbe1cab49b5c4aecd391397993c3cfc6c5
BeReyes1 <148413240+BeReyes1@users.noreply.github.com>
2025-05-30 15:21:05 -0700

    Full gameplay

--- src/Scenes/FollowBall.js
+++ src/Scenes/FollowBall.js
@@ -9,6 +9,7 @@ class FollowBall extends Phaser.Scene {
         this.load.image("playerBall", "blueball.png");
         this.load.image("aiBall", "pinkball.png");
         this.load.image("defaultBall", "defaultball.png");
+        this.load.image("arrowDown", "navigation_s.png");
     }
 
 
@@ -27,6 +28,15 @@ class FollowBall extends Phaser.Scene {
         //AI
         this.randomGuessSpeed = 1000; //when the balls move too fast for the ai 
         this.lastTargetSwap;
+
+        //Round data
+        this.totalRounds = 10;
+        this.currentRound = 1;
+
+        //Round score
+        this.roundScore = 1; //how many points each round is
+        this.playerScore = 0;
+        this.aiScore = 0; 
 
         //set balls and let them be clickable
         for (let i = 0; i < this.numBalls; i++) {
@@ -51,6 +61,25 @@ class FollowBall extends Phaser.Scene {
             this.aiTargetBall = this.balls[Phaser.Math.Between(0, this.numBalls - 1)];
         }
         this.aiTargetBall.setTexture("aiBall");
+
+        //pops up for AI guess
+        this.aiIndicator = this.add.image(100, 100, "arrowDown");
+        this.aiIndicator.setScale(1); 
+        this.aiIndicator.setVisible(false);
+        this.aiIndicator.setDepth(1);
+
+        //Text score
+        this.playerScoreText = this.add.text(20, 20, 'Player Score: 0', {
+            fontSize: '24px',
+            fill: '#0000ff',
+            fontFamily: 'Arial'
+        });
+
+        this.aiScoreText = this.add.text(20, 50, 'AI Score: 0', {
+            fontSize: '24px',
+            fill: '#ff00ff',
+            fontFamily: 'Arial'
+        });
 
         // After 1.5s, start the game!
         this.time.delayedCall(1500, () => {
@@ -122,9 +151,12 @@ class FollowBall extends Phaser.Scene {
 
         if (ball === this.targetBall) {
             console.log("Correct!");
+            this.playerScore += this.roundScore;
         } else {
             console.log("Incorrect");
         }
+
+        this.UpdateScores();
 
         //wait then prepare next round
         this.time.delayedCall(1500, () => {
@@ -170,18 +202,66 @@ class FollowBall extends Phaser.Scene {
             }
         }
 
+         if (this.balls.indexOf(guess) == this.balls.indexOf(this.aiTargetBall)) {
+            this.aiScore += this.roundScore;
+        }
+
         console.log("AI guessed ball at index: " + this.balls.indexOf(guess));
         console.log("Correct ball was at index: " + this.balls.indexOf(this.aiTargetBall));
         this.aiGuess = guess;
+        this.ShowAIPick(this.aiGuess);
+    }
+
+    ShowAIPick(ball) {
+        // Remove previous indicator if it exist
+
+        const arrowOffset = 60;
+        const x = ball.x;
+        const y = ball.y - arrowOffset;
+
+         this.aiIndicator.setPosition(x, y);
+        this.aiIndicator.setVisible(true);
     }
 
     //function for moving to next round
     PrepareNextRound() {
-        //Hide balls
+        this.currentRound++;
+        this.roundScore++;
+
+        //Hide balls+indicator
         this.balls.forEach(ball => ball.setTexture("defaultBall"));
+        this.aiIndicator.setVisible(false);
+
+        console.log("Current Round" + this.currentRound);
+
+        if (this.currentRound > this.totalRounds) {
+             this.add.text(400, 200, "Game Over!", { fontSize: '64px', color: '#ffa500' }).setOrigin(0.5);
+             
+            if (this.playerScore > this.aiScore) {
+                this.add.text(400, 100, "You Win!", { fontSize: '64px', color: '#ffa500' }).setOrigin(0.5);
+            }else if (this.aiScore > this.playerScore){
+                this.add.text(400, 100, "You Lose!", { fontSize: '64px', color: '#ffa500' }).setOrigin(0.5);
+            }else {
+                this.add.text(400, 100, "Draw", { fontSize: '64px', color: '#ffa500' }).setOrigin(0.5);
+            }
+
+            this.add.text(400, 300, "Press R to Restart", { fontSize: '32px', color: '#ffa500' }).setOrigin(0.5);
+            this.input.keyboard.once('keydown-R', () => {
+                this.scene.start('FollowBall'); 
+            });
+
+            return;
+
+        }
+
 
         this.time.delayedCall(300, () => {
             this.StartSwapping();
         });
+    }
+
+    UpdateScores() {
+        this.playerScoreText.setText('Player Score: ' + this.playerScore);
+        this.aiScoreText.setText('AI Score: ' + this.aiScore);
     }
 }
